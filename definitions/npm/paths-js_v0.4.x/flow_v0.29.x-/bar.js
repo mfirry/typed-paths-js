b1cac37deb887d7ee8c8eff5fc1513117534b489
mfirry <mfirry@gmail.com>
2016-10-05 16:53:10 +0200

    minor

--- definitions/npm/paths-js_v0.4.x/flow_v0.29.x-/bar.js
+++ definitions/npm/paths-js_v0.4.x/flow_v0.29.x-/bar.js
@@ -2,13 +2,13 @@ declare module "paths-js/bar" {
 
   declare class Bar<A> {
     constructor(options:
-      { data: Array<Array<A>>,
+      {| data: Array<Array<A>>,
         accessor?: (a: A) => number,
         width: number,
         height: number,
         max: number,
         min: number,
-        gutter?: number }): Bar<A>;
+        gutter?: number |}): Bar<A>;
   }
 
 }
